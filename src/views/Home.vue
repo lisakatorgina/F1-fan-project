b43c5a22001696fd8f5ae2249548eb0b4875690c
Lisa Katorgina <lisakatorgina@Lisas-MacBook-Pro.local>
2021-12-20 19:29:31 -0600

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
-    <video width="100%" height="100%" autoplay muted loop>
+    <video width="100%" height="100%" autoplay muted loop playsinline>
       <source src="../assets/videobg.mp4" type="video/mp4">
       <source src="../assets/videobg.ogg" type="video/ogg">
     </video>
-    <Intro/>
     <a href="https://www.instagram.com/lisalovesformula1/" class="inst" target="_blank">
       <img class="qr" src="../assets/img/qr.png" alt=""/>
       <span>@lisalovesformula1</span>
     </a>
+    <Intro/>
   </div>
 </template>
 
