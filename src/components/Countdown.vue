c8ade0e02d6e2273b29f6324e5ea5abe3322c30d
Lisa Katorgina <lisakatorgina@Lisas-MacBook-Pro.local>
2021-12-21 23:21:14 -0600

    more photos for gallery

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -2,11 +2,11 @@
   <div class="countdown">
     <countdown :end-time="new Date(2022, 2, 17, 18, 0, 0)">
       <template v-slot:process="countdown">
-        <h1>Loading in progress...</h1>
+        <h1>New season is loading...</h1>
         <div class="load-bar">
           <div class="load-bar__progress" :style="{width: `${Math.round(100 - (100/93 * countdown.timeObj.d))}%`}">{{ `${Math.round(100 - (100/93 * countdown.timeObj.d))}%` }}</div>
         </div>
-        <h2>The new racing season starts in</h2>
+        <h2>The next racing weekend starts in</h2>
         <div class="countdown__wrap">
           <div>
             <span>{{ countdown.timeObj.d }}</span>
