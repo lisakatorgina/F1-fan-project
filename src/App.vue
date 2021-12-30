98b5018b32cc3a7e1b5c441479c0d0ec56915496
Lisa Katorgina <lisakatorgina@Lisas-MacBook-Pro.local>
2021-12-29 21:50:22 -0600

    add more tracks and sharing

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <site-menu/>
     <router-view/>
+    <div :class="['sharing', `sharing_${$route.name.toLowerCase()}`]">
+      <span class="sharing__title">Share</span>
+      <div class="ya-share2" data-curtain data-size="s" data-lang="en" data-services="facebook,twitter"></div>
+    </div>
   </div>
 </template>
 
