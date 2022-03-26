9c65c7f28d29bf934ef0aa732426c1c237012235
Lisa Katorgina <lisakatorgina@Lisas-MacBook-Pro.local>
2022-03-26 13:20:59 -0600

    saudi arabia quali results

--- src/data/results.js
+++ src/data/results.js
@@ -1,6 +1,6 @@
 export default {
     1: {
-        past: false,
+        past: true,
         quali: ['lec', 'ver', 'sai', 'per', 'ham', 'bot', 'mag', 'alo', 'rus', 'gas', 'oco', 'msc', 'nor', 'alb', 'zho', 'tsu', 'hul', 'ric', 'str', 'lat'],
         scores: 'default',
         race: ['lec', 'sai', 'ham', 'rus', 'mag', 'bot', 'oco', 'tsu', 'alo', 'zho', 'msc', 'str', 'alb', 'ric', 'nor', 'lat', 'hul'],
@@ -10,7 +10,7 @@ export default {
     },
     2: {
         past: false,
-        //quali: [],
+        quali: ['per', 'lec', 'sai', 'ver', 'oco', 'rus', 'alo', 'bot', 'gas', 'mag', 'nor', 'ric', 'zho', 'msc', 'str', 'ham', 'alb', 'hul', 'lat', 'tsu'],
         //sprint: [],
         //scores: 'default',
         //race: [],
