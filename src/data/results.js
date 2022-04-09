14d3571777b1661cd88ba5b7e0982c2fcc290722
Lisa Katorgina <lisakatorgina@Lisas-MacBook-Pro.local>
2022-04-09 02:48:02 -0500

    australia quali results

--- src/data/results.js
+++ src/data/results.js
@@ -9,13 +9,23 @@ export default {
         descr: 'Seb got covid and was replaced by Nico. Safety car after Pierre engine burnt. Both Red Bull had car failure on the last laps.',
     },
     2: {
-        past: false,
+        past: true,
         quali: ['per', 'lec', 'sai', 'ver', 'oco', 'rus', 'alo', 'bot', 'gas', 'mag', 'nor', 'ric', 'zho', 'msc', 'str', 'ham', 'alb', 'hul', 'lat', 'tsu'],
         //sprint: [],
         scores: 'default',
         race: ['ver', 'lec', 'sai', 'per', 'rus', 'oco', 'nor', 'gas', 'mag', 'ham', 'zho', 'hul', 'str'],
         out: ['alb', 'bot', 'alo', 'ric', 'lat', 'tsu'],
         lap: 'lec',
-        descr: 'Nico is still replacing Seb. Heavy crash of Mick on quali didn\'t let him to race on sunday. Checo qualified brilliantly on pole and had been leading the race but lost his position after an unlucky pit stop one lap before the safety car which appeared because of Nicolas crash. Valtteri, Fernando and Daniel had car failures. Yuki was unable to start the race due to car issues. Intense fight between Max and Charles for the lead in the last laps.'
+        descr: 'Nico is still replacing Seb. Massive crash of Mick on quali didn\'t let him to race on sunday. Checo qualified brilliantly on pole and had been leading the race but lost his position after an unlucky pit stop one lap before the safety car which appeared because of Nicolas crash. Valtteri, Fernando and Daniel had car failures. Yuki was unable to start the race due to car issues. Intense fight between Max and Charles for the lead in the last laps.'
+    },
+    3: {
+        past: false,
+        quali: ['lec', 'ver', 'per', 'nor', 'ham', 'rus', 'ric', 'oco', 'sai', 'alo', 'gas', 'bot', 'tsu', 'zho', 'msc', 'alb', 'mag', 'vet', 'lat', 'str'],
+        //sprint: [],
+        scores: 'default',
+        //race: ['ver', 'lec', 'sai', 'per', 'rus', 'oco', 'nor', 'gas', 'mag', 'ham', 'zho', 'hul', 'str'],
+        //out: ['alb', 'bot', 'alo', 'ric', 'lat', 'tsu'],
+        //lap: 'lec',
+        descr: 'Quali: Nicolas and Lance collided. Fernando engine failed.'
     },
 }
