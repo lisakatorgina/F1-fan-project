3f49aa7359f97bf8986afb2e2e3da36fd0dd3c9e
Elizaveta Katorgina <elizaveta.katorgina@dalee.ru>
2022-10-03 14:20:43 -0500

    remove swiper gallery

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import App from './App.vue';
 import router from './router';
 import vueAwesomeCountdown from 'vue-awesome-countdown';
 import { Swiper as SwiperClass, Pagination, Mousewheel, Autoplay, Navigation, EffectFade } from 'swiper/swiper.esm';
-import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter';
+//import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter';
 import VueScreenSize from 'vue-screen-size';
 
 Vue.config.productionTip = false;
 Vue.use(vueAwesomeCountdown, 'vac');
 SwiperClass.use([Pagination, Mousewheel, Autoplay, Navigation, EffectFade]);
-Vue.use(getAwesomeSwiper(SwiperClass));
+//Vue.use(getAwesomeSwiper(SwiperClass));
 Vue.use(VueScreenSize);
 
 new Vue({
