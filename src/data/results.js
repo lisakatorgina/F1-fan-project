e591e922edae88b58c05036e03301d88cd186b62
Elizaveta Katorgina <lisakatorgina@gmail.com>
2023-03-21 22:02:39 -0600

    jeddah results

--- src/data/results.js
+++ src/data/results.js
@@ -1,6 +1,6 @@
 export default {
     1: {
-        past: false,
+        past: true,
         quali: ['ver', 'per', 'lec', 'sai', 'alo', 'rus', 'ham', 'str', 'oco', 'hul', 'nor', 'bot', 'zho', 'tsu', 'alb', 'sar', 'mag', 'pia', 'dev', 'gas'],
         scores: 'default',
         race: ['ver', 'per', 'alo', 'sai', 'ham', 'str', 'rus', 'bot', 'gas', 'alb', 'tsu', 'sar', 'mag', 'dev', 'hul', 'zho', 'nor'],
@@ -9,4 +9,13 @@ export default {
         //descr: '',
         lapNotScored: true,
     },
+    2: {
+        past: false,
+        quali: ['per', 'lec', 'alo', 'rus', 'sai', 'str', 'oco', 'ham', 'pia', 'gas', 'hul', 'zho', 'mag', 'bot', 'ver', 'tsu', 'alb', 'dev', 'nor', 'sar'],
+        scores: 'default',
+        race: ['per', 'ver', 'alo', 'rus', 'ham', 'sai', 'lec', 'oco', 'gas', 'mag', 'tsu', 'hul', 'zho', 'dev', 'pia', 'sar', 'nor', 'bot'],
+        out: ['alb', 'str'],
+        lap: 'ver',
+        //descr: '',
+    }
 }
